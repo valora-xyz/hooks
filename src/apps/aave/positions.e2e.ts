6da12b89295d30fe92ec512648350a84b557fa05
Tom McGuire <mcgtom10@gmail.com>
2026-01-15 18:54:07 -0800

    test: re-enable skipped tests

--- src/apps/aave/positions.e2e.ts
+++ src/apps/aave/positions.e2e.ts
@@ -2,9 +2,7 @@ import hook from './positions'
 import { NetworkId } from '../../types/networkId'
 import { t } from '../../../test/i18next'
 
-// TODO: Re-enable when migrated to Alchemy
-// eslint-disable-next-line jest/no-disabled-tests -- disabled temporarily because the api is returning errors
-describe.skip.each([NetworkId['arbitrum-one'], NetworkId['ethereum-mainnet']])(
+describe.each([NetworkId['arbitrum-one'], NetworkId['ethereum-mainnet']])(
   'getPositionDefinitions for networkId %s',
   (networkId) => {
     it('should get the address definitions successfully', async () => {
